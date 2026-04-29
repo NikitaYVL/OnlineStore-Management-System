48c88ddae391628a1679c3f95300b50c7b3bce6f
NikitaYVL <aestetgg@gmail.com>
2026-04-30 03:30:01 +0500

    изменения в feature-branch

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace СуторминУчетЗаявок
-{
+{/// обнова
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
